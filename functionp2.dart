35738cda2675d2370403c37b51ecfc0d6793162c
Sahil-Sham <sahilsham173@gmail.com>
2024-01-31 12:33:18 +0500

    homework practice

--- functionp2.dart
+++ functionp2.dart
@@ -1,19 +1,16 @@
 void main (){
-var abc =studentMarkStudent();
-print('abc :$abc');
+studentMarkStudent('sahil',45,65,76);
+studentMarkStudent('mudasir',78,80,67);
+studentMarkStudent('ali',44,56,89);
+
 }
-studentMarkStudent(){
-  String name= "Sahil";
-  var subject1=67;
-  var subject2=89;
-  var subject3=88;
+studentMarkStudent(String name,num subject1,subject2, subject3){
   var total_marks=subject1+subject2+subject3;
   var percentage=(total_marks/300)*100;
   if (percentage>55) {
-    print("$name pass");
+    print("$name pass or percentage $percentage");
     
   }else{
-    print("$name fail");
+    print("$name fail or percentage $percentage");
   }
-  return name;
 }
