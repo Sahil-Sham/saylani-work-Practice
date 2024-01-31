a098c79ff817a5bd60d62f790d8a711039ebb8eb
Sahil-Sham <sahilsham173@gmail.com>
2024-01-31 13:30:28 +0500

    homework practice

--- functionp3.dart
+++ functionp3.dart
@@ -3,11 +3,12 @@ teacherData(
   experience: "2 year",
   name: "ayesha",
   id: 123);
+  teacherData(
+  experience: "2 year",
+  name: "ayesha");
 }
-teacherData({
-  required String name,
-required String experience,
-required int id})
+         // requiured name        //required name         // dafault value
+teacherData({required String name,required String experience,int id =12})
 {
   print("name $name : experience $experience & i'd $id ");
 }
