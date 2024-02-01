29e0e657e9265450c92aa0b05820d66ec767dc9a
Sahil-Sham <sahilsham173@gmail.com>
2024-02-01 12:31:45 +0500

    homework practice

---
+++ functionp6.dart
@@ -0,0 +1,11 @@
+void main(){
+  onlinePlatform(name: 'Sahil',link: "facebook.com");
+}
+
+
+onlinePlatform({required String name,String? link} ){
+  String a = name;
+  print(a);
+String? ui = link ;
+print(ui);
+}
