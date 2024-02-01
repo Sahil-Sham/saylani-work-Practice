d46bc9564cfb4c6c85595fbcca0358b0a2255e9b
Sahil-Sham <sahilsham173@gmail.com>
2024-02-01 10:31:59 +0500

    homework practice

--- functionp4.dart
+++ functionp4.dart
@@ -1,14 +1,9 @@
-void main(){
-  List <int> numbers=[2,3,4,5,10,14];
-for (var i in numbers) {
-  numberTable(i);
-  
+
+void main (){
+printPersonInfo({'name':'john','age':30});
+}
+printPersonInfo(Map<String, dynamic> person) {
+  print("${person["name"]} is ${person["age"]} years old.");
+ 
 }
 
-}
-numberTable(int n ){
-  for (var i = 1; i < 10; i++) {
-    print("$n x $i =${n*i} ");
-  }
-  print("================");
-}
