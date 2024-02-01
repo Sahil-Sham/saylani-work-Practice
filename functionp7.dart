f8c9c70e8f935ac10f06a6cb7c318bdecdd69dba
Sahil-Sham <sahilsham173@gmail.com>
2024-02-01 22:05:48 +0500

    homework practice

---
+++ functionp7.dart
@@ -0,0 +1,8 @@
+void main(){
+var value=calculateArea(10.0,7.0);
+print("this is ractangular value :$value");
+}
+calculateArea(double lenght, double width){
+  print("this is lenght $lenght && this is width $width");
+  return lenght*width;
+}
