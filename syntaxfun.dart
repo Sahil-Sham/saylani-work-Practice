ee44769d3acf7c33df7de81ba40c76c7e2ec3a54
Sahil-Sham <sahilsham173@gmail.com>
2024-02-01 22:15:52 +0500

    homework practice

--- syntaxfun.dart
+++ syntaxfun.dart
@@ -1,10 +1,15 @@
+// void main(){
+// //  there we call the function :
+// abcd();
+// }
+// // there we create  a specific function :
+// abcd(){
+//   print("123");
+// // In the around brace we give a parameter:
+// // In the curly brace we give a statement :
+// }
+
 void main(){
-//  there we call the function :
-abcd();
-}
-// there we create  a specific function :
-abcd(){
-  print("123");
-// In the around brace we give a parameter:
-// In the curly brace we give a statement :
+  var abc = "sahil";
+  print(abc);
 }
