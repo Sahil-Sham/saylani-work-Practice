4e0b788b796a1495bba868756d52d9898d1da7f6
Sahil-Sham <sahilsham173@gmail.com>
2024-02-20 23:22:36 +0500

    done

--- practice2.dart
+++ practice2.dart
@@ -1,7 +1,14 @@
 
 
-void main (){
-  List <String> studentNames=["Sahil","Cheeku","Waqar","Ali","Mudassir","Suhan","Hamza","Moiz"];
-  studentNames.removeRange(5, 8);
-  print(studentNames);
+// void main (){
+//   List <String> studentNames=["Sahil","Cheeku","Waqar","Ali","Mudassir","Suhan","Hamza","Moiz"];
+//   studentNames.removeRange(5, 8);
+//   print(studentNames);
+// }
+void main(){
+  List <String> studentNames=["Sahil","Cheeku","Waqar","Ali","Mudassir"];
+  List<int> studentScore=[85, 90, 75, 80, 95];
+  for (var i = 0; i < studentNames.length; i++) {
+    print("${studentNames[i]} : ${studentScore[i]}");
+  }
 }
