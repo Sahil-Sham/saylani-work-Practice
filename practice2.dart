6633c7c49e9c77d0c1f4b3d1949168492982d179
Sahil-Sham <sahilsham173@gmail.com>
2024-02-20 23:43:34 +0500

    done

--- practice2.dart
+++ practice2.dart
@@ -5,10 +5,26 @@
 //   studentNames.removeRange(5, 8);
 //   print(studentNames);
 // }
+// void main(){
+//   List <String> studentNames=["Sahil","Cheeku","Waqar","Ali","Mudassir"];
+//   List<int> studentScore=[85, 90, 75, 80, 95];
+//   for (var i = 0; i < studentNames.length; i++) {
+//     print("${studentNames[i]} : ${studentScore[i]}");
+//   }
+// }
+
+import 'dart:io';
+
 void main(){
-  List <String> studentNames=["Sahil","Cheeku","Waqar","Ali","Mudassir"];
-  List<int> studentScore=[85, 90, 75, 80, 95];
-  for (var i = 0; i < studentNames.length; i++) {
-    print("${studentNames[i]} : ${studentScore[i]}");
+var abc =calculateAverage();
+print(abc);
+}
+calculateAverage(){
+  stdout.write("Enter the score:");
+  String score=stdin.readLineSync()!;
+
+  List<int> number=score.split(" ").map((String) => int.parse(String)).toList();
+  for (var i = 0; i < number.length; i++) {
   }
+return number;
 }
