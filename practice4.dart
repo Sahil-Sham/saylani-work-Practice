b9fc259a232cd33ca6e5a367c9734a7b82e19035
Sahil-Sham <sahilsham173@gmail.com>
2024-02-23 00:59:51 +0500

    done

--- practice4.dart
+++ practice4.dart
@@ -3,10 +3,16 @@
 //   print(myVar);
 // }
 
-void main(){
-  List<int> numbersList=[1,2,3,4,5];
-  for (var i = 0; i < numbersList.length; i++) {
-    print(numbersList[i]);
-  }
+// void main(){
+//   List<int> numbersList=[1,2,3,4,5];
+//   for (var i = 0; i < numbersList.length; i++) {
+//     print(numbersList[i]);
+//   }
   
+// }
+
+void main(){
+  Map<String,dynamic> student={"name":"suhan","age":20};
+  print("Name:${student["sahil"]}");
+  print("age:${student["age"]}");
 }
