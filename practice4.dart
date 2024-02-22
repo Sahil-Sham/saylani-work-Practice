2493c8eb761d155f2abace3189044154096fec10
Sahil-Sham <sahilsham173@gmail.com>
2024-02-23 00:42:14 +0500

    done

--- practice4.dart
+++ practice4.dart
@@ -1,4 +1,12 @@
+// void main(){
+//   int myVar=42;
+//   print(myVar);
+// }
+
 void main(){
-  int myVar=42;
-  print(myVar);
+  List<int> numbersList=[1,2,3,4,5];
+  for (var i = 0; i < numbersList.length; i++) {
+    print(numbersList[i]);
+  }
+  
 }
