dd1d81008a27001b606bf7382335b988f5fe1cde
Sahil-Sham <sahilsham173@gmail.com>
2024-02-26 12:59:28 +0500

    done

--- oop.dart
+++ oop.dart
@@ -1,24 +1,35 @@
-void main(){
-  print("========");
-  Student obj1= Student();
-  obj1.name="sahil";
-  obj1.studentInfor(true);
-  print("========");
-   Student obj2= Student();
-  obj2.name="waqar";
-  obj2.studentInfor(false);
+// void main(){
+//   print("========");
+//   Student obj1= Student();
+//   obj1.name="sahil";
+//   obj1.studentInfor(true);
+//   print("========");
+//    Student obj2= Student();
+//   obj2.name="waqar";
+//   obj2.studentInfor(false);
 
-}
-class Student {
-  String? name;
+// }
+// class Student {
+//   String? name;
 
 
-studentInfor(bool projectsubmited){
-if (projectsubmited) {
-  print("$name  is projct submitted");
-} else {
-  print("$name  is not submitted");
+// studentInfor(bool projectsubmited){
+// if (projectsubmited) {
+//   print("$name  is projct submitted");
+// } else {
+//   print("$name  is not submitted");
   
+// }
+// }
+// }
+void main(){
+for (var details in detail.values) {
+  print(details);
 }
 }
+enum detail{
+ my,
+ name,
+ sahil
+
 }
