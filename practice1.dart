2843fe8b9aa86a64235eb3f2e722eb8b6b5ac5ea
Sahil-Sham <sahilsham173@gmail.com>
2024-02-28 13:07:12 +0500

    done

--- practice1.dart
+++ practice1.dart
@@ -1,9 +1,26 @@
-void main(){
-  var percentage= 40 ;
-  if (percentage>50) {
-    print("pass");
+// void main(){
+//   var percentage= 40 ;
+//   if (percentage>50) {
+//     print("pass");
  
-  }else{
-    print("fail");
-  }
+//   }else{
+//     print("fail");
+//   }
+// }
+
+import 'dart:io';
+
+void main(){
+ int ractangle= calculateArea();
+  print("This is Area of ractangle:$ractangle");
+}
+calculateArea(){
+ stdout.write("Enter the Lenght:");
+ int lenght=int.parse(stdin.readLineSync()!);
+ stdout.write("Enter the widht:");
+ int widht=int.parse(stdin.readLineSync()!);
+
+ return lenght*widht;
+
+
 }
