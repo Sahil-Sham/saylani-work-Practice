8736fc05ef254bcf9b63b82032401975fb8bfe28
Sahil-Sham <sahilsham173@gmail.com>
2024-03-04 13:24:19 +0500

    done

--- practice6.dart
+++ practice6.dart
@@ -1,12 +1,34 @@
+// void main(){
+//   var per=person();
+//   per.name;
+//   print(per);
+// }
+// class person{
+// String? name;
+// String? age;
+// }
+// class sahil{
+//   String? gender;
+// }
+import 'dart:io';
+
 void main(){
-  var per=person();
-  per.name;
-  print(per);
-}
-class person{
-String? name;
-String? age;
-}
-class sahil{
-  String? gender;
+  stdout.write("Enter the Numbers:");
+  String numbers=stdin.readLineSync()!;
+  List<int> input=numbers.split(" ").map((String) => int.parse(String)).toList();
+  List<int> even=[];
+  List<int> odd=[];
+  for (var number in input) {
+    if (number %2==0) {
+      even.add(number);
+    }else{
+      odd.add(number);
+    }
+    
+  }
+  print("List of Numbers:$input");
+  print("This is EvenNumber:$even");
+  print("This is OddNumber:$odd");
+   
+
 }
