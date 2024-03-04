88ccdddf3428ae419be67040f847b4cd779bfc9a
Sahil-Sham <sahilsham173@gmail.com>
2024-03-04 13:43:32 +0500

    done

--- practice6.dart
+++ practice6.dart
@@ -10,25 +10,37 @@
 // class sahil{
 //   String? gender;
 // }
+// import 'dart:io';
+
+// void main(){
+//   stdout.write("Enter the Numbers:");
+//   String numbers=stdin.readLineSync()!;
+//   List<int> input=numbers.split(" ").map((String) => int.parse(String)).toList();
+//   List<int> even=[];
+//   List<int> odd=[];
+//   for (var number in input) {
+//     if (number %2==0) {
+//       even.add(number);
+//     }else{
+//       odd.add(number);
+//     }
+    
+//   }
+//   print("List of Numbers:$input");
+//   print("This is EvenNumber:$even");
+//   print("This is OddNumber:$odd");
+// }
 import 'dart:io';
 
 void main(){
-  stdout.write("Enter the Numbers:");
-  String numbers=stdin.readLineSync()!;
-  List<int> input=numbers.split(" ").map((String) => int.parse(String)).toList();
-  List<int> even=[];
-  List<int> odd=[];
-  for (var number in input) {
-    if (number %2==0) {
-      even.add(number);
-    }else{
-      odd.add(number);
-    }
-    
-  }
-  print("List of Numbers:$input");
-  print("This is EvenNumber:$even");
-  print("This is OddNumber:$odd");
-   
+   stdout.write("Enter the First Number:");
+  int first=int.parse(stdin.readLineSync()!);
+   stdout.write("Enter the Second Number:");
+  int second=int.parse(stdin.readLineSync()!);
+  int result=calculateSum(first, second);
+  print("The Sum Of First and Second Numbers:$result");
 
+}
+calculateSum(int num1, int num2){
+  return num1 + num2;
 }
