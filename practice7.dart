99ccaa5c7259f3b22d5b14dbbb6b16d37f93ea8a
Sahil-Sham <sahilsham173@gmail.com>
2024-03-05 13:24:41 +0500

    done

--- practice7.dart
+++ practice7.dart
@@ -1,11 +1,20 @@
-void main() {
-  // Creating a map named student
-  Map<String, dynamic> student = {
-    "name": "John Doe",  // Replace with your name
-    "age": 25            // Replace with your age
-  };
+// void main() {
+//   // Creating a map named student
+//   Map<String, dynamic> student = {
+//     "name": "John Doe",  // Replace with your name
+//     "age": 25            // Replace with your age
+//   };
 
-  // Printing values using keys
-  print("Name: ${student["name"]}");
-  print("Age: ${student["age"]}");
+//   // Printing values using keys
+//   print("Name: ${student["name"]}");
+//   print("Age: ${student["age"]}");
+// }
+
+void main() {
+  List<int> numberList = [1, 2, 3, 4, 5];
+
+  // Using a loop to print each element of the list
+  for (int number in numberList) {
+    print(number);
+  }
 }
