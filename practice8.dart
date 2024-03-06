10bc80ef11eee9da07909048908833efa75bc3c9
Sahil-Sham <sahilsham173@gmail.com>
2024-03-06 13:07:36 +0500

    done

--- practice8.dart
+++ practice8.dart
@@ -1,7 +1,26 @@
-void main(){
-int result=multiply(4, 5);
-print("This is product:$result");
+// void main(){
+// int result=multiply(4, 5);
+// print("This is product:$result");
+// }
+// multiply(int a, int b){
+// return a*b;
+// }
+
+
+void main() {
+  // Create a list of integers
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Find the sum of elements in the list
+  int sum = calculateSum(numbers);
+
+  // Print the result
+  print('Sum of elements in the list: $sum');
 }
-multiply(int a, int b){
-return a*b;
+
+int calculateSum(List<int> numbers) {
+  // Use the reduce method to calculate the sum of elements in the list
+  int sum = numbers.reduce((value, element) => value + element);
+
+  return sum;
 }
