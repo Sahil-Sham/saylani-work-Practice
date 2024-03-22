9f74df3c7308ce73450f129369f79a38cbd0ef59
Sahil-Sham <sahilsham173@gmail.com>
2024-03-22 13:09:24 +0500

    done

--- inheritance.dart
+++ inheritance.dart
@@ -1,6 +1,7 @@
 void main(){
 var men=Men("Male","sahil",21);
-
+int age=10;
+men.perDaywork();
 
 }
 class Human{
@@ -9,12 +10,24 @@ class Human{
   Human(this.name,this.age){ 
   print("hello From human");
   }
+  perDaywork(){
+    print("Eat");
+    print("Sleep");
+    print("Survive");
+  }
    }
   class Men extends Human{
     String gender="Male";
     Men(this.gender,String n,int a): super(n,a){
        print("hello From Men");
     }
+    @override
+  perDaywork() {
+   print("Eat");
+    print("work");
+    print("Survive");
+  
+  }
   }
   // class Female extends Human{
   //   String gender="Female";
